b675e92aac5ca7c94924800bcd5e84bba84a0115
chalgoman <chalgoman@gmail.com>
2015-10-27 21:55:21 +0200

    initial commit

---
+++ udri.js
@@ -0,0 +1,122 @@
+var renderer = PIXI.autoDetectRenderer(852, 387, { backgroundColor: 0xAAAAAA, transparent: false });
+document.body.appendChild(renderer.view);
+var stage = new PIXI.Container();
+PIXI.ticker.shared.add(function (time) {
+	renderer.render(stage);
+});
+
+var loader = new PIXI.loaders.Loader();
+for (var i = 0; i < 3; i++) {
+	loader.add('img/plesnica'+i+'.png','img/plesnica'+i+'.png');
+	loader.add('img/umruk'+i+'.png','img/umruk'+i+'.png');
+}
+loader.once('complete', loaded);
+loader.load();
+
+var counters = {
+	plesnici: 0,
+	umruci: 0
+};
+
+var plesnica, umruk;
+
+var buttonUmruk = document.getElementById('umruk');
+var buttonPlesnica = document.getElementById('plesnica');
+
+// a function which scales the canvas to fit in the window
+var resizeCanvas = function() {
+	var width = window.innerWidth,
+		height = window.innerHeight,
+		scale = Math.min(width / renderer.view.width, height / renderer.view.height);
+
+	var newWidth = renderer.view.width * scale,
+		newHeight = renderer.view.height * scale;
+
+	renderer.view.style.width = newWidth + 'px';
+	renderer.view.style.height = newHeight + 'px';
+	renderer.view.style.left = ((width - newWidth) / 2) + 'px';
+	renderer.view.style.top = 90 + 'px';
+	document.body.style.width = window.innerWidth + 'px';
+	document.body.style.height = window.innerHeight + 'px';
+	buttonUmruk.style.height = window.innerHeight - newHeight - 90 + 'px';
+	buttonPlesnica.style.height = window.innerHeight - newHeight - 90 + 'px';
+	document.getElementById('addthis').style.top = newHeight + 62 + 'px';
+};
+
+// rescale the canvas on window resize
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+
+function initMovieClips() {
+	var framesPlesnica = [];
+	var framesUmruk = [];
+	for (var i = 0; i < 3; i++) {
+		framesPlesnica.push(PIXI.Texture.fromImage('img/plesnica' + i + '.png'));
+		framesUmruk.push(PIXI.Texture.fromImage('img/umruk' + i + '.png'));
+	}
+	
+	plesnica = new PIXI.extras.MovieClip(framesPlesnica);
+	plesnica.loop = false;
+	plesnica.visible = false;
+	plesnica.animationSpeed = 0.15;
+
+	umruk = new PIXI.extras.MovieClip(framesUmruk);
+	umruk.loop = false;
+	umruk.visible = false;
+	umruk.animationSpeed = 0.15;
+
+	stage.addChild(plesnica);
+	stage.addChild(umruk);
+	plesnica.onComplete = function() {
+		plesnica.gotoAndStop(0);
+		addScore('plesnica');
+		activateButtons();
+	}
+	umruk.onComplete = function() {
+		umruk.gotoAndStop(0);
+		addScore('umruk');
+		activateButtons();
+	}
+}
+
+function udariUmruk() {
+	deactivateButtons();
+	plesnica.visible = false;
+	umruk.visible = true;
+	umruk.gotoAndPlay(0);
+}
+
+function udariPlesnica() {
+	deactivateButtons();
+	umruk.visible = false;
+	plesnica.visible = true;
+	plesnica.gotoAndPlay(0);
+}
+
+function addScore( type ) {
+	console.warn('add score', type);
+	if ( type === 'umruk' ) {
+		counters.umruci++;
+		document.getElementById('counterUmruci').innerHTML = 'Юмруци: ' + counters.umruci;
+	} else {
+		counters.plesnici++;
+		document.getElementById('counterPlesnici').innerHTML = 'Плесници: ' + counters.plesnici;
+	}
+}
+
+function deactivateButtons() {
+	buttonUmruk.disabled = true;
+	buttonPlesnica.disabled = true;
+}
+
+function activateButtons() {
+	buttonUmruk.disabled = false;
+	buttonPlesnica.disabled = false;
+}
+
+function loaded() {
+	initMovieClips();
+	document.body.removeChild(document.getElementById('preloader'));
+	umruk.gotoAndStop(0);
+	umruk.visible = true;
+};
